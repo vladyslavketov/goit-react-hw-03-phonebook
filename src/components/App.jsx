5a7a31aacb991f3bc2db729e16c1f43f305f8613
vladyslavketov <106628608+vladyslavketov@users.noreply.github.com>
2023-01-02 20:47:59 +0200

    hw-03 | done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,24 @@ import ContactsList from './ContactsList/ContactsList';
 
 class App extends Component {
   state = {
-    contacts:  [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
+    contacts:  [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contactsFromLS = JSON.parse(localStorage.getItem('contacts'));
+
+    if (contactsFromLS) {
+      this.setState({ contacts: contactsFromLS });
+      console.log(contactsFromLS);
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = (name, number) => {
     const newContact = { id: nanoid(), name, number };
